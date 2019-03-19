b6e474268b2d2638f8ee2ec69229d239bb511f07
JinZ1999 <cetacean1999@gmail.com>
2019-03-19 13:48:48 -0400

    update

--- Assets/scr_pause.cs
+++ Assets/scr_pause.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scr_pause : MonoBehaviour
 {
     [SerializeField]
     public GameObject pausePanel;
+    public AudioSource music;
+    public AudioSource sfx;
     void Start()
     {
         pausePanel.SetActive(false);
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (!pausePanel.activeInHierarchy)
             {
@@ -27,14 +30,35 @@ public class scr_pause : MonoBehaviour
     private void PauseGame()
     {
         Time.timeScale = 0;
-        Debug.Log("space!~");
         pausePanel.SetActive(true);
         //Disable scripts that still work while timescale is set to 0
     }
     private void ContinueGame()
     {
         Time.timeScale = 1;
-        Debug.Log("start!~");
+        PlayerPrefs.SetFloat ("music", music.volume);
+        int i;
+        if (music.enabled)
+        {
+            i = 1;
+        } else
+        {
+            i = 0;
+        }
+        PlayerPrefs.SetInt("music_enable", i);
+
+        PlayerPrefs.SetFloat("sfx", sfx.volume);
+        int j;
+        if (sfx.enabled)
+        {
+            j = 1;
+        }
+        else
+        {
+            j = 0;
+        }
+        PlayerPrefs.SetInt("sfx_enable", j);
+        PlayerPrefs.Save();
         pausePanel.SetActive(false);
         //enable the scripts again
     }
--- Assets/scripts/scr_manager.cs
+++ Assets/scripts/scr_manager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class scr_manager : MonoBehaviour
 {
@@ -9,11 +10,46 @@ public class scr_manager : MonoBehaviour
     float speed = 2.0f;
     public GameObject[] prefab;
     bool gameStarted;
+    public AudioSource music;
+    public AudioSource sfx1;
+    public AudioSource sfx2;
 
     // Start is called before the first frame update
     void Start()
     {
         gameStarted = false;
+        if (PlayerPrefs.HasKey("music"))
+        {
+            music.volume = PlayerPrefs.GetFloat("music");
+        }
+        if (PlayerPrefs.HasKey("music_enable"))
+        {
+            if(PlayerPrefs.GetInt("music_enable") == 1)
+            {
+                music.enabled = true;
+            } else
+            {
+                music.enabled = false;
+            }
+        }
+        if (PlayerPrefs.HasKey("sfx"))
+        {
+            sfx1.volume = PlayerPrefs.GetFloat("sfx");
+            sfx2.volume = PlayerPrefs.GetFloat("sfx");
+        }
+        if (PlayerPrefs.HasKey("sfx_enable"))
+        {
+            if (PlayerPrefs.GetInt("sfx_enable") == 1)
+            {
+                sfx1.enabled = true;
+                sfx2.enabled = true;
+            }
+            else
+            {
+                sfx1.enabled = false;
+                sfx2.enabled = false;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -55,6 +91,7 @@ public class scr_manager : MonoBehaviour
             gameStarted = false;
             CancelInvoke();
             savescore(score);
+            SceneManager.LoadScene("End", LoadSceneMode.Single);
         }
     }
 
@@ -73,8 +110,21 @@ public class scr_manager : MonoBehaviour
         }
         if (PlayerPrefs.HasKey(tmp))
         {
+            Rec(tmp);
             PlayerPrefs.SetInt(tmp, s);
         }
         PlayerPrefs.Save();
+    }
+
+    void Rec(string tmp)
+    {
+        if (tmp.Length == 10)
+        {
+            return;
+        } else
+        {
+            Rec(tmp + "*");
+            PlayerPrefs.SetInt(tmp+"*", PlayerPrefs.GetInt(tmp));
+        }
     }
 }
